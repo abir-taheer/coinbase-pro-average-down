464f26b0c5c857a6429b4ba18fc20f345fe0b56a
Abir Taheer <abir@taheer.me>
2021-06-08 05:09:37 -0400

    Revert "only logging high price changes of 1% or more"
    This reverts commit 5421711963b2abeee74c15be4077a9bbf12328ab.

--- state.js
+++ state.js
@@ -6,18 +6,15 @@ let targetPrice = 0;
 let isProcessing = false;
 
 const setHighestPrice = (num) => {
-  const percentDiff = highestPrice ? (num - highestPrice) / highestPrice : 1;
   highestPrice = num;
   targetPrice = calculateTargetPrice(num);
 
-  if (percentDiff > 0.01) {
-    console.log(
-      `New high price set to:`,
-      chalk.green(num.toFixed(2)),
-      "\tTarget price: ",
-      chalk.red(targetPrice.toFixed(2))
-    );
-  }
+  console.log(
+    `New high price set to:`,
+    chalk.green(num.toFixed(2)),
+    "\tTarget price: ",
+    chalk.red(targetPrice.toFixed(2))
+  );
 };
 
 const getHighestPrice = () => highestPrice;
