5421711963b2abeee74c15be4077a9bbf12328ab
Abir Taheer <abir@taheer.me>
2021-06-08 03:30:27 -0400

    only logging high price changes of 1% or more

--- state.js
+++ state.js
@@ -6,15 +6,18 @@ let targetPrice = 0;
 let isProcessing = false;
 
 const setHighestPrice = (num) => {
+  const percentDiff = highestPrice ? (num - highestPrice) / highestPrice : 1;
   highestPrice = num;
   targetPrice = calculateTargetPrice(num);
 
-  console.log(
-    `New high price set to:`,
-    chalk.green(num.toFixed(2)),
-    "\tTarget price: ",
-    chalk.red(targetPrice.toFixed(2))
-  );
+  if (percentDiff > 0.01) {
+    console.log(
+      `New high price set to:`,
+      chalk.green(num.toFixed(2)),
+      "\tTarget price: ",
+      chalk.red(targetPrice.toFixed(2))
+    );
+  }
 };
 
 const getHighestPrice = () => highestPrice;
